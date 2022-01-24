8e43e99a7a54dbb3d174462c8d0102299864877b
lh <jyfc7879@gmail.com>
2022-01-24 12:48:23 +0800

    ...

--- src/main/java/com/yunmo/auth/spring/ExceptionHandling.java
+++ src/main/java/com/yunmo/auth/spring/ExceptionHandling.java
@@ -2,20 +2,38 @@ package com.yunmo.auth.spring;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.zalando.problem.Problem;
 import org.zalando.problem.Status;
+import org.zalando.problem.StatusType;
 import org.zalando.problem.spring.web.advice.ProblemHandling;
+import org.zalando.problem.spring.web.advice.security.SecurityAdviceTrait;
+
+import java.util.Optional;
 
 
 @ControllerAdvice
-public class ExceptionHandling implements ProblemHandling {
-    @ExceptionHandler(BadCredentialsException.class)
+public class ExceptionHandling implements ProblemHandling , SecurityAdviceTrait {
+
+    @ExceptionHandler
     public ResponseEntity<Problem> handleEntityNotFound(
             final BadCredentialsException exception,
             final NativeWebRequest request) {
         return create(Status.UNAUTHORIZED, exception, request);
     }
+
+    @ExceptionHandler
+    public ResponseEntity<Problem> handleEntityNotFound(
+            final DisabledException exception,
+            final NativeWebRequest request) {
+        return ResponseEntity.status(Status.UNAUTHORIZED.getStatusCode()).body(Problem.builder()
+                .withDetail("账户已禁用")
+                .withStatus(Status.UNAUTHORIZED)
+                .withTitle(Status.UNAUTHORIZED.getReasonPhrase())
+                .build());
+    }
+
 }
