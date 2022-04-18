dedcd39286539f45d75c6f108ac01e926d06dc6c
lh <jyfc7879@gmail.com>
2022-04-18 16:04:19 +0800

    add phone login

--- src/main/java/com/yunmo/auth/spring/user/UserDetailServiceImpl.java
+++ src/main/java/com/yunmo/auth/spring/user/UserDetailServiceImpl.java
@@ -4,15 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -20,26 +18,53 @@ import org.springframework.stereotype.Service;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class UserDetailServiceImpl implements UserDetailsService {
     public static final String DOMAIN_DELIMITER  = "/";
 
-    public static final String SELECT_USER_ACCOUNT_SQL = "SELECT  id, account_name, enabled, password FROM user_account as ua where ua.account_name = ?";
+    public static final String SELECT_USER_ACCOUNT_SQL_BY_ACCOUNT_NAME = "SELECT  id, account_name, enabled, password FROM user_account as ua where ua.account_name = ?";
+
+    public static final String SELECT_USER_ACCOUNT_SQL_BY_STAFF_ID = "SELECT  id, account_name, enabled, password FROM user_account as ua where ua.staff_id = ?";
+
+    public static final String SELECT_STAFF_BY_PHONE = "SELECT  id  FROM staff  where phone = ?";
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    public Staff getStaffId(String phone) {
+        try {
+            return jdbcTemplate.queryForObject(SELECT_STAFF_BY_PHONE, new BeanPropertyRowMapper<Staff>(Staff.class), phone);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        } catch (IncorrectResultSizeDataAccessException e) {
+            throw new IncorrectResultSizeDataAccessException("员工数据存在多条对应记录",1);
+        }
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserAccount userAccount = null;
         try {
-            userAccount = jdbcTemplate.queryForObject(SELECT_USER_ACCOUNT_SQL, new BeanPropertyRowMapper<UserAccount>(UserAccount.class),username);
+            userAccount = jdbcTemplate.queryForObject(SELECT_USER_ACCOUNT_SQL_BY_ACCOUNT_NAME, new BeanPropertyRowMapper<UserAccount>(UserAccount.class),username);
         } catch (EmptyResultDataAccessException e) {
-            throw new UsernameNotFoundException("用户名不存在");
+        }
+
+        if (userAccount == null) {
+           try {
+               Staff staff = getStaffId(username);
+               if (staff == null) {
+                   throw new UsernameNotFoundException("用户名不存在");
+               }
+               try {
+                   userAccount = jdbcTemplate.queryForObject(SELECT_USER_ACCOUNT_SQL_BY_STAFF_ID, new BeanPropertyRowMapper<UserAccount>(UserAccount.class), staff.getId());
+               } catch (IncorrectResultSizeDataAccessException e) {
+                   throw new IncorrectResultSizeDataAccessException("账号数据存在多条对应员工编号",1);
+               }
+           } catch (EmptyResultDataAccessException e) {
+               throw new UsernameNotFoundException("用户名不存在");
+           }
         }
 
         String[] segments = username.split(DOMAIN_DELIMITER); //支持username中加入domain
@@ -79,5 +104,13 @@ public class UserDetailServiceImpl implements UserDetailsService {
         private String password;
         private String accountName;
         private boolean enabled;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    private static class Staff{
+        private Long id;
     }
 }
